b32f7715c791483db1ca77c6f910c021dafae1ea
shaneburke <shaneburke0@gmail.com>
2016-09-21 12:36:45 +1000

    Setup scripts and styles directories

--- DeepAncestry.Web/App_Start/BundleConfig.cs
+++ DeepAncestry.Web/App_Start/BundleConfig.cs
@@ -8,24 +8,15 @@ namespace DeepAncestry.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/libs").Include(
+                        "~/Scripts/libs/modernizr-2.6.2.js",
+                        "~/Scripts/libs/angular.min.js",
+                        "~/Scripts/libs/jquery-1.10.2.min.js",
+                        "~/Scripts/libs/angular-route.min.js",
+                        "~/Scripts/libs/angular-resource.min.js",
+                        "~/Scripts/libs/bootstrap.min.js",
+                        "~/Scripts/libs/respond.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
         }
     }
 }
