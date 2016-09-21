01642a8279abac910917c044a05bc4d6ad0edf0f
shaneburke <shaneburke0@gmail.com>
2016-09-21 12:49:16 +1000

    Implemented api controller to search for people

--- DeepAncestry.Web/Controllers/api/SearchController.cs
+++ DeepAncestry.Web/Controllers/api/SearchController.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using DeepAncestry.Web.Services;
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -9,9 +8,54 @@ namespace DeepAncestry.Web.Controllers.api
 {
     public class SearchController : ApiController
     {
-        public SearchController()
-        {
+        private readonly ISearchService _searchService;
 
+        public SearchController(ISearchService searchService)
+        {
+            if (searchService == null)
+            {
+                throw new ArgumentNullException("searchService");
+            }
+
+            _searchService = searchService;
+        }
+
+        public HttpResponseMessage Get(string name, string gender)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                var result = _searchService.FindPerson(name, gender);
+
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        public HttpResponseMessage Post(string name, string gender, string direction)
+        {
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(gender) || string.IsNullOrWhiteSpace(direction))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                var result = _searchService.FindFamily(name, gender, direction);
+
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
